97a2e44a51050b283665a1baba721b2cd1c2811a
Raviraj <113274453+raviraj4@users.noreply.github.com>
2024-02-21 18:28:44 +0530

    added math mod examples

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"math"
+)
+// function to solve the fibonacci
 func fibo(num int) {
-
 	if num < 1 {
 		fmt.Println(num)
 	}
@@ -15,16 +17,46 @@ func fibo(num int) {
 		fmt.Println(b)
 	}
 }
+// function using the math module
+func trig(arg float64) (float64, float64, float64) {
+	SinA := func(angle float64) float64 {
+		return math.Sin(angle)
+	}
+
+	CosA := func(angle float64) float64 {
+		return math.Cos(angle)
+	}
+	TanA := func(angle float64) float64 {
+		return math.Tan(angle)
+	}
+	
+	res1 := SinA(arg)
+	res2 := CosA(arg)
+	res3 := TanA(arg)
+
+	return res1, res2, res3
+}
 
 func main() {
-	fibo(10)
-	
-	// declaring (variable, int): 
-	// var name = "arnab"
-	// var number int = 69
-
+	 
+	 fmt.Println(trig(45))
 	// printing in golang
 	// fmt.Println("number is , ", number)
+
+	// array
+	var arr1 [2]int
+	arr1[0] = 1
+	arr1[1] = 3
+	fmt.Println(arr1)
+
+	// DATATYPES IN GOLANG
+	// var name = "arnab"
+	// var number int = 69
+	// var isMale bool = true
+	// var number float64 = 69.3
+
+	// FIBONAACCI
+	// fibo(10)
 
 	// fmt.Println("good morning, " + name)
 }
